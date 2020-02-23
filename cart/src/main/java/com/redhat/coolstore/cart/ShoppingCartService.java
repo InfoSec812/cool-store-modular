580d779347fab5af114aa5747077eb26c639b442
Deven Phillips <deven.phillips@redhat.com>
2020-02-22 22:01:44 -0500

    Initial import of modular project

---
+++ cart/src/main/java/com/redhat/coolstore/cart/ShoppingCartService.java
@@ -0,0 +1,9 @@
+package com.redhat.coolstore.cart;
+
+import com.redhat.coolstore.model.ShoppingCart;
+
+public interface ShoppingCartService {
+		
+	public void priceShoppingCart(ShoppingCart sc);
+	
+}
